60f7d3ec6bdb8e53cfdb2d022aa6f0289e99fb36
bqureshi94 <60623098+bqureshi94@users.noreply.github.com>
2022-04-06 03:57:26 -0500

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,7 +2,7 @@ export const projects = [
   {
     title: 'Movieflix',
     description: "Using React, Node.js, and OMDB API, I have build this basic movie application where users can type the name of any movie in the search bar and the webite will return the results based on their search",
-      image: '/images/1.PNG',
+      image: '/images/1.png',
       tags: ['React', 'Node', 'OMDB API'],
     source: 'https://github.com/bqureshi94/Movieflix',
     visit: 'https://bqureshi94.github.io/Movieflix/',
@@ -11,7 +11,7 @@ export const projects = [
   {
     title: 'ProNeft',
     description: "Using React, and Tailwindcss I have build a showcase website for a React Native app. This app is crossplatform where users can show case their nfts as well as trade with each other. Follow up for more",
-      image: '/images/2.PNG',
+      image: '/images/2.png',
       tags: ['React', 'Tailwindcss'],
     source: 'https://github.com/bqureshi94/Movieflix',
     visit: 'https://bqureshi94.github.io/Movieflix/',
